e48f17e3fdf47c01841238893db33627c462bbbd
irgc <irgc@vaclavs-MacBook-Pro.local>
2022-02-24 10:35:53 +0100

    isl1

---
+++ slices/counterSlice.js
@@ -0,0 +1,14 @@
+import { createSlice } from "@reduxjs/toolkit"
+
+const inicialState = {
+    value: 0
+}
+export const counterSlice = createSlice(
+    {
+        name: 'counter',
+        initialState,
+        reducers: {
+            
+        }
+    }
+)
